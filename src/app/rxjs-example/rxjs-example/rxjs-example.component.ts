d21c5d32e1e024fbf8b97087d6edc512886ab2af
profanis <fanis@projectbeagle.com>
2019-10-10 21:51:08 +0300

    rxjs 2 problem

--- src/app/rxjs-example/rxjs-example/rxjs-example.component.ts
+++ src/app/rxjs-example/rxjs-example/rxjs-example.component.ts
@@ -1,28 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RxjsService } from '../rxjs.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-example',
   templateUrl: './rxjs-example.component.html',
   styleUrls: ['./rxjs-example.component.scss']
 })
-export class RxjsExampleComponent implements OnInit, OnDestroy {
+export class RxjsExampleComponent implements OnInit {
 
   items: number[];
-  subscription: Subscription;
   constructor(private rxjsService: RxjsService) { }
 
   ngOnInit() {
-    this.subscription = this.rxjsService.getItems().subscribe(data => {
-      this.items = data;
-    });
-  }
 
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
   }
 
 }
