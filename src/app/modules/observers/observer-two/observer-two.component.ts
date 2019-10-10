6d3beda4327f16842abe94e4ade87b23a1f69939
profanis <fanis@projectbeagle.com>
2019-10-10 21:28:00 +0300

    unsubscribe

--- src/app/modules/observers/observer-two/observer-two.component.ts
+++ src/app/modules/observers/observer-two/observer-two.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ObserverService } from '../observer.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observer-two',
   templateUrl: './observer-two.component.html',
   styleUrls: ['./observer-two.component.scss']
 })
-export class ObserverTwoComponent implements OnInit {
+export class ObserverTwoComponent implements OnInit, OnDestroy {
 
 
   observerData: string;
+  subscription: Subscription;
 
   constructor(private observerService: ObserverService) { }
 
 
   ngOnInit() {
-    this.observerService.ping.subscribe(data => {
+    this.subscription = this.observerService.ping.subscribe(data => {
       this.observerData = data;
     });
   }
 
   triggerObserver() {
     this.observerService.propagateData();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
