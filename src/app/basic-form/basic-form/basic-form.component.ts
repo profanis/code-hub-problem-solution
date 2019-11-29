a9ba6afab6c7a1fe8ef6857ae7c9561ac7db1a3c
profanis <prodromouf@gmail.com>
2019-11-29 23:08:22 +0200

    reactive form complete example

--- src/app/basic-form/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -7,15 +7,34 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss']
 })
 export class BasicFormComponent implements OnInit {
-  form: FormGroup
+  form: FormGroup;
+  programmingLanguages = ['TS', 'JS', 'C#'];
 
   constructor() { }
 
   ngOnInit() {
     this.form = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl()
-    })
+      firstName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      lastName: new FormControl(null, Validators.required),
+      isExperienced: new FormControl(null, Validators.required),
+      angular: new FormControl(null, Validators.required),
+      favouriteLanguage: new FormControl(null, Validators.required),
+      jsversion: new FormControl(null, Validators.required),
+    });
+
+
+    this.form.get('favouriteLanguage').valueChanges.subscribe(value => {
+
+      const jsVersionFormControl = this.form.get('jsversion');
+
+      if (value === 'JS') {
+        jsVersionFormControl.setValidators(Validators.required);
+      } else {
+        jsVersionFormControl.clearValidators();
+      }
+      jsVersionFormControl.updateValueAndValidity();
+    });
+
   }
 
 }
