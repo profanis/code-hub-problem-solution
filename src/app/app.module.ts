6e852a9bb4f47fa706ff6707277de9412dc4b3c0
profanis <prodromouf@gmail.com>
2020-01-28 23:38:38 +0200

    directives 1st problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
   ],
   imports: [
     BrowserModule
