3879e6e97802a5e7dc3759f408074dcc42d2ab3f
profanis <prodromouf@gmail.com>
2020-01-28 23:14:15 +0200

    pipes 3rd problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TextTransformPipe } from './pipes/text-transform.pipe';
-import { DateTransformPipe } from './pipes/date-transform.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextTransformPipe,
-    DateTransformPipe
   ],
   imports: [
     BrowserModule
