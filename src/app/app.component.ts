d60bb34d6bceaa29f8ff765ad1d547bb8b2d61cc
profanis <prodromouf@gmail.com>
2020-01-28 22:57:30 +0200

    pipes 2nd solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'convert this to uppercase';
+  title = 'convert THIS TO lowercase';
 }
