aa4dd207739ff9bcd2deb43aadd88678a679e509
profanis <prodromouf@gmail.com>
2020-01-29 17:38:42 +0200

    http interceptor 1st problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyHttpInterceptorService } from './my-http-interceptor.service';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,11 +13,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MyHttpInterceptorService,
-      multi: true
-    }
   ],
   bootstrap: [AppComponent]
 })
